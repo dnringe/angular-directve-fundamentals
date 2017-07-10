6a7af2856c2f75175efa99f45d037de660cd1698
Dattatraya Ringe <dnringe@gmail.com>
2017-07-10 22:03:19 +0530

    aboutUsCard updated

--- src/app/components/aboutus/aboutus.controller.js
+++ src/app/components/aboutus/aboutus.controller.js
@@ -9,8 +9,8 @@
   function AboutusController() {
     var vm = this;
     vm.msg = "this is aboutus";
-    vm.user = {
-      name: 'sudarshan',
+    vm.user1 = {
+      name: 'Sudarshan',
       address: {
         street: 'PO Box 123',
         city: 'select Rebel Base',
@@ -22,6 +22,22 @@
         'Chewbacca'
       ]
     }
+    /*vm.user2 = {
+      name: 'Prasanna',
+      address: {
+        street: 'PO Box 123',
+        city: 'select Rebel Base',
+        planet: 'Yavin 4'
+      },
+      friends: [
+        'Han',
+        'Leia',
+        'Chewbacca'
+      ]
+    }*/
+
+
+
 
   }
 })();
@@ -32,7 +48,33 @@
     .directive('userInfoCard', function () {
       return {
         templateUrl: "app/components/aboutus/userInfoCard.html",
-        restrict: "E"
+        restrict: "E",
+        scope: {
+          user: '='
+        },
+        controller: function ($scope) {
+          $scope.vm.collapsed = false;
+          $scope.knightMe = function (user) {
+            $scope.vm.user.rank = "knight";
+          }
+          $scope.vm.collapse = function () {
+            $scope.vm.collapsed = !$scope.vm.collapsed;
+          }
+        },
+        controllerAs: 'vm',
+        bindToController: true
       }
-    })
+    });
+  angular.module('angularDirectveFundamentals').directive('address', function () {
+    return {
+      templateUrl: "app/components/aboutus/address.html",
+      restrict: "E",
+      controller: function ($scope) {
+
+      },
+      controllerAs: 'vm',
+      bindToController: true
+    }
+  });
+
 })();
